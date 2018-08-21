359470c469c3f48cef43531d4c27e4630ce52aeb
Dustin Gooding <dgooding@houstonmechatronics.com>
2018-08-21 09:17:43 -0700

    added some extra cflags to enforce gnu compiler options

--- rv.sh
+++ rv.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y build-essential cmake
 
 mkdir build
 cd build
-cmake -DCMAKE_C_COMPILER=kcc -DCMAKE_C_FLAGS="-fissue-report=./my_errors.json" ..
+cmake -DCMAKE_C_COMPILER=kcc -DCMAKE_C_FLAGS="-std=gnu11 -DPACKED='__attribute__((__packed__))' -DPACKED_BEGIN='' -DPACKED_END='' -fissue-report=./my_errors.json" ..
 make
 
 # no executable at this time
